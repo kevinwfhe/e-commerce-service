e650687679b840d0cc1323e1db631e664615d963
kevin-7755 <valarmorghulis_kh@outlook.com>
2022-03-14 18:42:54 -0400

    feat(): add ServiceAggregator to seperate services injection

--- Utils/ServiceAggregator.cs
+++ Utils/ServiceAggregator.cs
@@ -4,7 +4,11 @@ using csi5112group1project_service.Services;
 public class ServicesAggregator {
   // Mount services to the application.
   public static WebApplicationBuilder AddServices(WebApplicationBuilder builder) {
+    builder.Services.AddSingleton<AdminService>();
+    builder.Services.AddSingleton<ClientService>();
     builder.Services.AddSingleton<ProductService>();
+    builder.Services.AddSingleton<OrderService>();
+    builder.Services.AddSingleton<ShippingAddressService>();
     return builder;
   }
 }
